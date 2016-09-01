8d191c462707362dcd781daa392ad6e39c19b466
jackie <chenjie_nj@126.com>
2016-09-01 23:55:29 +0800

    TestTokenInfoServiceImp单元测试

---
+++ src/test/java/indi/jackie/wechat/service/impl/TestTokenInfoServiceImpl.java
@@ -0,0 +1,17 @@
+package indi.jackie.wechat.service.impl;
+
+/**
+ * @author jackie chen
+ * @create 2016/9/1
+ * @description 基础Service测试类
+ */
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/test/resource/spring-test.xml"})
+public class TestTokenInfoServiceImpl {
+
+}
